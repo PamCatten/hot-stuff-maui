fcfa30a02950f73b86defb62ebb3ed420bc5e531
Cam Patten <campatten@outlook.com>
2023-07-15 15:33:53 -0400

    Reconciled links to tabbed ContentTemplates.

---
+++ hot-stuff/Pages/ItemsPage.xaml.cs
@@ -0,0 +1,11 @@
+using UraniumUI.Pages;
+
+namespace hot_stuff.Pages;
+
+public partial class ItemsPage : UraniumContentPage
+{
+    public ItemsPage()
+    {
+        InitializeComponent();
+    }
+}

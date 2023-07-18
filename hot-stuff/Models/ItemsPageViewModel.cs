3733ac95a072f9ff2655cdd30be4acb6781a4c18
Cam Patten <campatten@outlook.com>
2023-07-18 18:17:49 -0400

    Refactored and restructured codebase again. Code is hopefully cleaner, but I seem to have made no progress in regards to actual functionality. So continues this voyage of damnation.

--- hot-stuff/Models/ItemsPageViewModel.cs
+++ hot-stuff/Models/ItemsPageViewModel.cs
@@ -7,40 +7,76 @@ using UraniumUI;
 
 namespace HotStuff.Models;
 
-[INotifyPropertyChanged]
-[QueryProperty("Item", "Item")]
-public partial class ItemsPageViewModel
+public partial class ItemsPageViewModel : BindableObject
 {
-    [ObservableProperty]
-    Item item;
-
-
-    public ObservableCollection<Item> Items { get; protected set; } = new ObservableCollection<Item>();
+    public ObservableCollection<Item> Items { get; set; } = new ObservableCollection<Item>();
     public ObservableCollection<Item> SelectedItems { get; set; } = new ObservableCollection<Item>();
 
-    private Item newItemPrivate = new();
-    public Item newItemPublic { get => newItemPrivate; set { newItemPrivate = value; OnPropertyChanged(); } }
+    private Item newItem = new();
+    public Item NewItem { get => newItem; set { newItem = value; OnPropertyChanged(); } }
 
-    public ICommand AddNewItemCommand { get; set; }
-    public ICommand RemoveSelectedItemCommand { get; protected set; }
-    public Item Item { get => item; set => item = value; }
-
-    [RelayCommand]
-    AddNewItemCommand = new Command(() =>
+    public ItemsPageViewModel()
     {
-        Items.Insert(0, newItemPublic);
-        newItemPublic = new();
-    });
+        foreach (var item in Items)
+        {
+            Items.Add(new Item 
+            {
+                ItemName = item.ItemName,
+                Category = item.Category,
+                Room = item.Room,
+                ItemVersion = item.ItemVersion,
+                ItemDescription = item.ItemDescription,
+                Color = item.Color,
+                AmountPaid = item.AmountPaid,
+                BrandManufacturer = item.BrandManufacturer,
+                PurchaseDate = item.PurchaseDate,
+                PurchaseProof = item.PurchaseProof,
+            });
+        }
+        Items.Add(new Item
+        {
+            ItemName = "Pride and Prejudice",
+            Category = ItemCategory.BooksMagazines,
+            BrandManufacturer = "Bantam Classics",
+            Room = ItemRoom.Library,
+            ItemVersion = "Perma-Bound Hardcover",
+            Color = ItemColor.Black,
+            PurchaseDate = "09/02/2017",
+            AmountPaid = 12.99m,
+            ItemDescription = "Written by Jane Austen",
+            PurchaseProof = "https://www.aws.com/exampleurl/"
+        });
+
+        Items.Add(new Item
+        {
+            ItemName = "Aug 2023 VOGUE Magazine",
+            Category = ItemCategory.BooksMagazines,
+            BrandManufacturer = "Vogue",
+            Room = ItemRoom.Library,
+            ItemVersion = "Aug 2023 Issue",
+            Color = ItemColor.White,
+            PurchaseDate = "07/17/2023",
+            AmountPaid = 3.99m,
+            ItemDescription = "August 2023 Issue, Olivia Rodrigo cover-model",
+            PurchaseProof = "https://www.aws.com/exampleurl/"
+        });
+    }
 
     [RelayCommand]
-    RemoveSelectedItemCommand = new Command(() =>
+    async Task AddNewItem()
+    {
+        Items.Insert(0, NewItem);
+        NewItem = new Item();
+    }
+
+    [RelayCommand]
+    async Task RemoveSelectedItem()
     {
         foreach (var item in SelectedItems)
         {
             Items.Remove(item);
         }
-
-    });
+    }
 }
 
 

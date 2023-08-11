22769d281ca1514c0f18e6adb7eea135be5b7082
Cam Patten <campatten@outlook.com>
2023-08-11 11:40:02 -0400

    Added checker to prevent runtime errors when trying to add more than one item to a room in the ActiveBuilding RoomValue. Should sum correctly now and, uh, not crash.

--- hot-stuff/ViewModel/MainPageViewModel.cs
+++ hot-stuff/ViewModel/MainPageViewModel.cs
@@ -9,7 +9,6 @@ using SkiaSharp;
 using UraniumUI;
 
 namespace HotStuff.ViewModel;
-
 public class MainPageViewModel : UraniumBindableObject
 {
     private Building activeBuilding = new();
@@ -26,24 +25,29 @@ public class MainPageViewModel : UraniumBindableObject
             IgnoresBarPosition = true
         },
     };
+
+    public Dictionary<ItemRoom, decimal> ItemDictionary = new(); 
+
     public IEnumerable<ISeries> PieSeries { get; set; } =
     new[] { 2, 4, 1, 4, 3 }.AsPieSeries((value, series) =>
     {
         series.InnerRadius = 0;
-    });
 
+    });
     public MainPageViewModel()
     {
-        if (ActiveBuilding is not null)
+        try
         {
-            Debug.WriteLine("ActiveBuilding is not Null");
-            ActiveBuilding = new Building
+            if (ActiveBuilding is not null)
             {
-                BuildingID = 01,
-                BuildingName = "Patten House",
-                BuildingDescription = "A 3 bedroom, 2 bathroom home with a 3 car garage and a small backyard.",
-                BuildingType = BuildingType.House,
-                BuildingManifest = new ObservableCollection<Item>
+                // Test data for the ActiveBuilding
+                ActiveBuilding = new Building
+                {
+                    BuildingID = 01,
+                    BuildingName = "Test House",
+                    BuildingDescription = "A 3 bedroom, 2 bathroom home with a 3 car garage and a small backyard.",
+                    BuildingType = BuildingType.House,
+                    BuildingManifest = new ObservableCollection<Item>
                 {
                     new Item { ItemID = 43, ItemName = "Test Item 43", ItemQuantity = 1, Room = ItemRoom.Attic, ItemPrice = 191.00m, Category = ItemCategory.Antiques, ItemDescription = "Test Item", Color = ItemColor.Black, BrandManufacturer = "Test Company" },
                     new Item { ItemID = 44, ItemName = "Test Item 44", ItemQuantity = 1, Room = ItemRoom.LivingRoom, ItemPrice = 182.00m, Category = ItemCategory.Fixtures, ItemDescription = "Test Item", Color = ItemColor.Black, BrandManufacturer = "Test Company" },
@@ -51,24 +55,39 @@ public class MainPageViewModel : UraniumBindableObject
                     new Item { ItemID = 46, ItemName = "Test Item 46", ItemQuantity = 2, Room = ItemRoom.Garage, ItemPrice = 45.99m, Category = ItemCategory.SportsEquipment, ItemDescription = "Test Item", Color = ItemColor.Black, BrandManufacturer = "Test Company" },
                     new Item { ItemID = 47, ItemName = "Test Item 47", ItemQuantity = 4, Room = ItemRoom.Entryway, ItemPrice = 76.00m, Category = ItemCategory.Lighting, ItemDescription = "Test Item", Color = ItemColor.Black, BrandManufacturer = "Test Company" },
                     new Item { ItemID = 48, ItemName = "Test Item 48", ItemQuantity = 5, Room = ItemRoom.Kitchen, ItemPrice = 33.99m, Category = ItemCategory.Lighting, ItemDescription = "Test Item", Color = ItemColor.Black, BrandManufacturer = "Test Company" },
-                    new Item { ItemID = 49, ItemName = "Test Item 49", ItemQuantity = 1, Room = ItemRoom.Bathroom, ItemPrice = 400.00m, Category = ItemCategory.Electronics, ItemDescription = "Test Item", Color = ItemColor.Black, BrandManufacturer = "Test Company" },
+                    new Item { ItemID = 49, ItemName = "Test Item 49", ItemQuantity = 1, Room = ItemRoom.Greenhouse, ItemPrice = 400.00m, Category = ItemCategory.Electronics, ItemDescription = "Test Item", Color = ItemColor.Black, BrandManufacturer = "Test Company" },
                 },
-                BuildingValue = 1.00m,
-                BuildingItemCount = 0,
-                BuildingRoomValue = new Dictionary<ItemRoom, decimal> { { ItemRoom.Attic, 0.00m }, { ItemRoom.Studio, 1.00m } },
-
-            };
-            Debug.WriteLine($"BuildingRoomValue = {ActiveBuilding.BuildingRoomValue}");
-            Debug.WriteLine($"ID: {ActiveBuilding.BuildingID} NAME: {ActiveBuilding.BuildingName} DESCRIPTION: {ActiveBuilding.BuildingDescription}");
-            Debug.WriteLine($"Building value: ${ActiveBuilding.BuildingValue}");
-            
+                    BuildingValue = 1.00m,
+                    BuildingItemCount = 0,
+                    BuildingRoomValue = new Dictionary<ItemRoom, decimal> {},
+                };
+            }
+            else
+            {
+                Debug.WriteLine("ActiveBuilding is null.");
+            }
         }
-        else
+        catch (Exception ex)
         {
-            Debug.WriteLine("ActiveBuilding is Null.");
-        };
+            Debug.WriteLine($"Error retrieving ActiveBuilding: {ex.Message}");
+            Shell.Current.DisplayAlert("Building Record Retrieval Error", ex.Message, "OK");
+        }
 
+        // TODO: This is gross, but it's good enough for now. I'm sure there's a better way to do this.
+        ActiveBuilding.BuildingRoomValue.Clear();
+        foreach (var item in ActiveBuilding.BuildingManifest)
+        {
+            if (ActiveBuilding.BuildingRoomValue.ContainsKey((ItemRoom)item.Room))
+                ActiveBuilding.BuildingRoomValue[(ItemRoom)item.Room] += item.ItemPrice;
+            else
+                ActiveBuilding.BuildingRoomValue.Add((ItemRoom)item.Room, item.ItemPrice);
+        }
+        ItemDictionary = ActiveBuilding.BuildingRoomValue;
+        Debug.WriteLine($"ItemDictionary: {ItemDictionary}");
 
-
+        foreach (KeyValuePair<ItemRoom, decimal> entry in ActiveBuilding.BuildingRoomValue)
+        {
+            Debug.WriteLine($"Key: {entry.Key}, Value: {entry.Value}");
+        }
     }
 }

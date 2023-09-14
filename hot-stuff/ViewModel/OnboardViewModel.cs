247357bae937f04d87a17e7b96059141ab54b8b6
Cam Patten <campatten@outlook.com>
2023-09-14 16:27:57 -0400

    Intermediary images created for onboard screens, learned a lot. Revisions needed to make the experience a little more seamless.

--- hot-stuff/ViewModel/OnboardViewModel.cs
+++ hot-stuff/ViewModel/OnboardViewModel.cs
@@ -10,6 +10,7 @@ public partial class OnboardViewModel : BaseViewModel
     public int Position { get => position; set { position = value; OnPropertyChanged(); } }
     public ICommand OpenLegalPopupCommand { get; protected set; }
     public ICommand OpenOnboardCarouselPopupCommand { get; protected set; }
+    public ICommand OpenAddBuildingPopupCommand { get; protected set; }
     public ICommand ClosePopupCommand { get; protected set; }
     public ICommand OnboardPositionCommand => new Command(async () =>
     {
@@ -23,34 +24,35 @@ public partial class OnboardViewModel : BaseViewModel
     {
         OpenLegalPopupCommand = new Command(async () => await MopupService.Instance.PushAsync(new LegalPopup(buildingService)));
         OpenOnboardCarouselPopupCommand = new Command(async () => await MopupService.Instance.PushAsync(new OnboardCarouselPopup(buildingService)));
+        OpenAddBuildingPopupCommand = new Command(async () => await MopupService.Instance.PushAsync(new AddBuildingPopup(buildingService)));
         ClosePopupCommand = new Command(async () => await MopupService.Instance.PopAsync());
 
         OnboardScreens.Add(new OnboardScreen
         {
-            OnboardTitle = "Get organized, stay secure",
+            OnboardTitle = "Organized, secure",
             OnboardDescription = "Welcome to Hot Stuff, your trusted companion in making proof-of-loss claims quick and easy. Add photos, descriptions, and values to ensure you're prepared for the unexpected.",
-            OnboardImage = "onboarddark"
+            OnboardImage = "onboard1"
         });
 
         OnboardScreens.Add(new OnboardScreen
         {
-            OnboardTitle = "Seamless data management",
+            OnboardTitle = "Seamless management",
             OnboardDescription = "Effortlessly download and transfer your records to Excel or Google Sheets for added convenience. You're in control of your data, always.",
-            OnboardImage = "onboarddark"
+            OnboardImage = "onboard2"
         });
 
         OnboardScreens.Add(new OnboardScreen
         {
             OnboardTitle = "Privacy first",
             OnboardDescription = "Designed with anonymity in mind, we never collect any of your personal information. Your data stays private, and we're here to help you protect it.",
-            OnboardImage = "onboarddark"
+            OnboardImage = "onboard3"
         });
 
         OnboardScreens.Add(new OnboardScreen
         {
             OnboardTitle = "Peace of mind, simplified",
             OnboardDescription = "Rest easy knowing that if a fire breaks out, your records are secured digitally, so you are free to focus all of your attention on protecting what matters most.",
-            OnboardImage = "onboarddark"
+            OnboardImage = "onboard4"
         });
     }
 }
